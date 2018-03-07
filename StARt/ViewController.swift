35d96720d862593217269df41dcffdbb811cf767
Califano Francesco <califanofrancesco@sal-04.homenet.telecomitalia.it>
2018-03-07 22:18:20 +0100

    Initial Commit

---
+++ StARt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StARt
+//
+//  Created by Califano Francesco on 07/03/18.
+//  Copyright © 2018 Califano Francesco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

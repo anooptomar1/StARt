937cb21f8ec75063cac870c4abd34fa847217532
Califano Francesco <califanofrancesco@sal-04.homenet.telecomitalia.it>
2018-03-07 22:40:37 +0100

    readded files

---
+++ StARt/BasketballCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasketballCollectionViewCell.swift
+//  EasyLearn
+//
+//  Created by Califano Francesco on 07/03/18.
+//  Copyright © 2018 Califano Francesco. All rights reserved.
+//
+
+import UIKit
+
+class BasketballCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ StARt/NerfCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NerfCollectionViewCell.swift
+//  EasyLearn
+//
+//  Created by Califano Francesco on 07/03/18.
+//  Copyright © 2018 Califano Francesco. All rights reserved.
+//
+
+import UIKit
+
+class NerfCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
